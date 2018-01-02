53ea79f5b8d7d008e76fbbf5d2893739fd78d17d
Yu Zhou <johnny0614@gmail.com>
2018-01-03 01:22:34 +1030

    add computeCostMulti

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -13,9 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+gap = X*theta - y;
+J = (gap'*gap)/(2*m);
 
 % =========================================================================
 
